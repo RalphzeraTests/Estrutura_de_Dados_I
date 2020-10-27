6a7d834b17338e947da096dbf75ffd86dcace324
Rodrigo Ferraz Souza <rodrigo.souza289@etec.sp.gov.br>
2020-10-27 16:47:01 -0300

    adicionei o quicksort

--- HeapSort/AbstractSort.h
+++ HeapSort/AbstractSort.h
@@ -48,72 +48,59 @@ void destroi(Elemento **umVetor, int quantidadeDeElementos)
 
     free(umVetor);
 }
-void printaVetor(Elemento **umVetor, int quantidadeDeElementos)
-{
-    for (int cont = 0; cont < quantidadeDeElementos; cont++)
-    {
-        printf("%d\n", umVetor[cont]->_chave);
-    }
-}
-
 void max_heapify(Elemento **umVetor, int quantidadeDeElementos, int i)
 {
     if (quantidadeDeElementos <= i || i < 0)
     {
-        //throw posicao_invalida_exception();
+        throw posicao_invalida_exception();
     }
-    printf("max_heapify\n");
-    printf("i: %d\n", i);
-    printaVetor(umVetor, quantidadeDeElementos);
-    int a = (i + 1) * 2;
-    int b = (i + 1) * 2 + 1;
-    int max = i + 1;
-    printf("esq: %d\ndir: %d\nmax: %d\n", a - 1, b - 1, max - 1);
+    //printf("max_heapify\n");
+    //printf("i: %d\n", i);
+    int a = (i * 2) + 1;
+    int b = (i * 2) + 2;
+    int max = i;
+    //printf("esq: %d\ndir: %d\nmax: %d\n", a, b, max);
 
-    printf("a\n");
-    if (a <= quantidadeDeElementos && umVetor[a - 1]->_chave > umVetor[i]->_chave)
+    //printf("a\n");
+    if (a < quantidadeDeElementos && umVetor[a]->_chave > umVetor[max]->_chave)
     {
         max = a;
     }
-    printf("b\n");
-    if (b <= quantidadeDeElementos && umVetor[b - 1]->_chave > umVetor[max - 1]->_chave)
+    //printf("b\n");
+    if (b < quantidadeDeElementos && umVetor[b]->_chave > umVetor[max]->_chave)
     {
         max = b;
     }
-    printf("c\n");
+    //printf("c\n");
     if (max != i)
     {
-        printf("max!=i -> max: %d\n", max);
-        swap(i - 1, max - 1, umVetor);
+        //printf("max!=i -> max: %d\n", max);
+        swap(i, max, umVetor);
         max_heapify(umVetor, quantidadeDeElementos, max);
-        printf("d\n");
+        //printf("d\n");
     }
 
-    printf("f\n");
+    //printf("f\n");
 }
 
 void construirHeapMax(Elemento **umVetor, int tamanho)
 {
-    printf("\nconstruirHeapMax\n");
-    printaVetor(umVetor, tamanho);
-    for (int i = (tamanho / 2); i > 0; i--)
+    //printf("\nconstruirHeapMax\n");
+    for (int i = (tamanho / 2) - 1; i >= 0; i--)
     {
-        max_heapify(umVetor, tamanho, i - 1);
+        max_heapify(umVetor, tamanho, i);
     }
 }
 void sort(Elemento **umVetor, int quantidadeDeElementos)
 {
-    printf("\nSORT\n");
-    printaVetor(umVetor, quantidadeDeElementos);
+    //printf("\nSORT\n");
     construirHeapMax(umVetor, quantidadeDeElementos);
-    for (int k = quantidadeDeElementos; k > 1; k--)
+    for (int k = quantidadeDeElementos - 1; k > 0; k--)
     {
-        printf("swaps:%d\n\n", getSwapsCount());
-        swap(0, k - 1, umVetor);
-        max_heapify(umVetor, k - 1, 0);
+        //printf("swaps:%d\n\n", getSwapsCount());
+        swap(0, k, umVetor);
+        max_heapify(umVetor, k, 0);
     }
-
-    printaVetor(umVetor, quantidadeDeElementos);
 }
 
 #endif /* ABSTRACTSORT_H */
