f357884b158e70298f7a43c1155ee7e3545bf0ce
Rodrigo Ferraz Souza <rodrigo.souza289@etec.sp.gov.br>
2020-10-29 00:18:19 -0300

    adicionei o insection

--- HeapSort/AbstractSort.h
+++ HeapSort/AbstractSort.h
@@ -54,38 +54,26 @@ void max_heapify(Elemento **umVetor, int quantidadeDeElementos, int i)
     {
         throw posicao_invalida_exception();
     }
-    //printf("max_heapify\n");
-    //printf("i: %d\n", i);
     int a = (i * 2) + 1;
     int b = (i * 2) + 2;
     int max = i;
-    //printf("esq: %d\ndir: %d\nmax: %d\n", a, b, max);
-
-    //printf("a\n");
     if (a < quantidadeDeElementos && umVetor[a]->_chave > umVetor[max]->_chave)
     {
         max = a;
     }
-    //printf("b\n");
     if (b < quantidadeDeElementos && umVetor[b]->_chave > umVetor[max]->_chave)
     {
         max = b;
     }
-    //printf("c\n");
     if (max != i)
     {
-        //printf("max!=i -> max: %d\n", max);
         swap(i, max, umVetor);
         max_heapify(umVetor, quantidadeDeElementos, max);
-        //printf("d\n");
     }
-
-    //printf("f\n");
 }
 
 void construirHeapMax(Elemento **umVetor, int tamanho)
 {
-    //printf("\nconstruirHeapMax\n");
     for (int i = (tamanho / 2) - 1; i >= 0; i--)
     {
         max_heapify(umVetor, tamanho, i);
@@ -93,7 +81,6 @@ void construirHeapMax(Elemento **umVetor, int tamanho)
 }
 void sort(Elemento **umVetor, int quantidadeDeElementos)
 {
-    //printf("\nSORT\n");
     construirHeapMax(umVetor, quantidadeDeElementos);
     for (int k = quantidadeDeElementos - 1; k > 0; k--)
     {
